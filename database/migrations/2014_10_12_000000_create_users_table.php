296fa68cf6e34416410ed123d42a4f5be9e238fb
Mohd.Haris <155016881+harislive@users.noreply.github.com>
2024-02-21 16:04:46 +0500

    Migration and Dummy Data
    Add Data

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role')->default(0); //role 0 User // Role 1 Admin
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    protected static ?string $password;
+
+    public function run(): void
+    {
+        User::create(
+            [
+                'name'=>'admin',
+                'email'=>'admin@example.com',
+                'email_verified_at' => now(),
+                'password' => static::$password ??= Hash::make('password'),
+                'remember_token' => Str::random(10),
+                'role'=>1,
+            ]);
+    }
+}
